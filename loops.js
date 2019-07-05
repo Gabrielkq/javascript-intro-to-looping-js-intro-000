ea77b40e3c937cca1a8c446a410857c21245f137
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-07-05 02:53:09 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,10 @@
 function forLoop(array){
-  for (let i = 0; 1 <25; i++){
+ /* for (let i = 0; 1 <25; i++){
     if (i===1){ 
       array.push("I am 1 strange loop.");
       
     }else array.push(`I am ${i} strange loops.`);
   }
+}*/
 }
 
